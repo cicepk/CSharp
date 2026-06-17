dd566725cea593d8c5f4d53b5ca1294cd9adc5c2
pk <phankiet8207@gmail.com>
2026-06-18 00:23:25 +0700

    feat (back): add docker

--- Back/TuneVault.API/Program.cs
+++ Back/TuneVault.API/Program.cs
@@ -109,12 +109,9 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TuneVault API v1"));
-}
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TuneVault API v1"));
 
 if (!app.Environment.IsDevelopment())
     app.UseHttpsRedirection();
