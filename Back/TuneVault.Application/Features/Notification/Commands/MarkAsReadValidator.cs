fa17720c88d98febbd8f62c9c7ad4e509ec6fbfb
pk <phankiet8207@gmail.com>
2026-06-25 01:33:37 +0700

    feat(back): add validator for share, noti, follow

---
+++ Back/TuneVault.Application/Features/Notification/Commands/MarkAsReadValidator.cs
@@ -0,0 +1,15 @@
+using FluentValidation;
+
+namespace TuneVault.Application.Features.Notification.Commands;
+
+public class MarkAsReadValidator : AbstractValidator<MarkAsReadCommand>
+{
+    public MarkAsReadValidator()
+    {
+        RuleFor(x => x.NotificationId)
+            .NotEmpty().WithMessage("NotificationId is required");
+
+        RuleFor(x => x.CurrentUserId)
+            .NotEmpty().WithMessage("CurrentUserId is required");
+    }
+}
